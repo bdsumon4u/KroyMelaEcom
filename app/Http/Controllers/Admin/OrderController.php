550b7c44c9f603e7d21ef1ba06dc159613e24e70
Sumon Ahmed <bdsumon4u@gmail.com>
2023-07-21 12:41:09 +0600

    Change Discount, Advanced, Shipping

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -63,6 +63,9 @@ class OrderController extends Controller
             'address' => 'required',
             'note' => 'nullable',
             'status' => 'required',
+            'data.discount' => 'required|integer',
+            'data.advanced' => 'required|integer',
+            'data.shipping_cost' => 'required|integer',
         ]);
 
         $order->update($data);
